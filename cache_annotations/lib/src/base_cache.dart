20be9bfda69555da51d2c29f8a51b77df479a3a4
Jerome Caudoux <caudoux.jerome@gmail.com>
2025-05-15 15:06:32 +0200

    [Lint] Remove unused imports

--- cache_annotations/lib/src/base_cache.dart
+++ cache_annotations/lib/src/base_cache.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 import 'package:cache_annotations/annotations.dart';
-import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
 
 abstract class BaseCache {
   Future<Directory> get directory;
